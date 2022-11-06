59e4d6c9d2c2f093a71a2ea7fa4df966724527dc
chubxu <chubxu@gmail.com>
2022-11-06 13:42:15 +0800

    feat: 实现最最最...基本的http通信能力

--- src/main/java/org/chubxu/mini/tomcat/Bootstrap.java
+++ src/main/java/org/chubxu/mini/tomcat/Bootstrap.java
@@ -1,18 +1,48 @@
 package org.chubxu.mini.tomcat;
 
+import org.chubxu.mini.tomcat.utils.HttpProtocolUtil;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * @ClassName Bootstrap
  * @Description mini tomcat 启动入口
- * @Since 1.0.0
+ * @Since 0.0.1
  * @Date 2022/11/6 11:37
  * @Author chubxu
  */
 public class Bootstrap {
     private int port = 8080;
 
+    private AtomicBoolean started = new AtomicBoolean(false);
 
     public static void main(String[] args) {
+        Bootstrap bootstrap = new Bootstrap();
+        try {
+            bootstrap.start();
+        } catch (IOException e) {
+            System.out.println(e.getMessage() + "\n" + e);
+            System.exit(-1);
+        }
+    }
 
+    public void start() throws IOException {
+        if (started.compareAndSet(false, true)) {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (started.get()) {
+                Socket socket = serverSocket.accept();
+                OutputStream os = socket.getOutputStream();
+                os.write(HttpProtocolUtil.contactHttpProtocol("Hello MiniTomcat").getBytes(StandardCharsets.UTF_8));
+                os.flush();
+                os.close();
+                socket.close();
+            }
+        }
     }
 
     public int getPort() {
---
+++ src/main/java/org/chubxu/mini/tomcat/utils/HttpProtocolUtil.java
@@ -0,0 +1,21 @@
+package org.chubxu.mini.tomcat.utils;
+
+/**
+ * @ClassName HttpProtocolUtil
+ * @Description 用于处理HTTP协议的工具类
+ * @Since 0.0.1
+ * @Date 2022/11/6 11:58
+ * @Author chubxu
+ */
+public class HttpProtocolUtil {
+    public static String contactHttpProtocol(String body) {
+        String header = "HTTP/1.1 200 OK \n";
+        String line = "Content-Type: text/html \n" +
+                "Content-Length: " + body.getBytes().length + "\n";
+        String blank = "\r\n";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(header).append(line).append(blank).append(body);
+        return sb.toString();
+    }
+}
